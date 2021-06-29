0746119bc46f9301ca07574213ca7d8974179239
Jerry He <jerry_he@trendmicro.com>
2021-06-29 16:23:30 +0800

    update json content

--- func_test_api_for_grafana/__init__.py
+++ func_test_api_for_grafana/__init__.py
@@ -7,21 +7,23 @@ import azure.functions as func
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    data = [
-        {'Block_1': {'install': True,
-                     'build': False,
-                     'l2_test': False}},
-        {'Block_2': {'install': False,
-                     'build': False,
-                     'l2_test': False}},
-        {'Block_3': {'install': False,
-                     'build': False,
-                     'l2_test': False}},
-        {'Block_4': {'install': False,
-                     'build': False,
-                     'l2_test': False}}
+    node_view = [
+        {'block_name': 'block 1',
+         'github_ee': 'mgcp',
+         'cicd_entry_tool': 'codebuild',
+         'stage_view': [{'id': 'init', 'status': 'running'},
+                        {'id': 'testing', 'status': 'waiting'},
+                        {'id': 'tear_down', 'status': 'stopping'}]
+        },
+        {'block_name': 'block 2',
+         'github_ee': 'ds',
+         'cicd_entry_tool': 'jenkins',
+         'stage_view': [{'id': 'init', 'status': 'waiting'},
+                        {'id': 'testing', 'status': 'waiting'},
+                        {'id': 'tear_down', 'status': 'waiting'}]
+        }
     ]
 
     func.HttpResponse.mimetype = 'application/json'
     func.HttpResponse.charset = 'utf-8'
-    return func.HttpResponse(json.dumps(data), status_code=200, mimetype='application/json', charset='utf-8')
+    return func.HttpResponse(json.dumps(node_view), status_code=200, mimetype='application/json', charset='utf-8')
