c84d5b37f7e57494a22523842f53f66ab5ad0108
Jerry He <jerry_he@trendmicro.com>
2021-06-29 14:50:26 +0800

    change format

--- func_test_api_for_grafana/__init__.py
+++ func_test_api_for_grafana/__init__.py
@@ -7,33 +7,21 @@ import azure.functions as func
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    data = {
-        'Block_1': {'install': True,
-                    'build': False,
-                    'l2_test': False},
-        'Block_2': {'install': False,
-                    'build': False,
-                    'l2_test': False},
-        'Block_3': {'install': False,
-                    'build': False,
-                    'l2_test': False},
-        'Block_4': {'install': False,
-                    'build': False,
-                    'l2_test': False}
-    }
+    data = [
+        {'Block_1': {'install': True,
+                     'build': False,
+                     'l2_test': False}},
+        {'Block_2': {'install': False,
+                     'build': False,
+                     'l2_test': False}},
+        {'Block_3': {'install': False,
+                     'build': False,
+                     'l2_test': False}},
+        {'Block_4': {'install': False,
+                     'build': False,
+                     'l2_test': False}}
+    ]
 
     func.HttpResponse.mimetype = 'application/json'
     func.HttpResponse.charset = 'utf-8'
-    if name:
-        return func.HttpResponse(json.dumps(data), status_code=200)
-    else:
-        return func.HttpResponse(json.dumps(data), status_code=200)
+    return func.HttpResponse(json.dumps(data), status_code=200, mimetype='application/json', charset='utf-8')
